5feeaf1090488941c899cc162ccfff7f7eed83db
taksas <takuminakayama4869@gmail.com>
2022-06-23 08:18:41 +0000

    [FINISHED] 2-1

---
+++ db/migrate/20220621024727_create_lists.rb
@@ -0,0 +1,13 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+
+
+      t.string :title
+      t.string :body
+
+
+      t.timestamps
+    end
+  end
+end
