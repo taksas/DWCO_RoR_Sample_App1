6a1ee32c77f5b7148604731d3a81eb6aa4ac4dad
taksas <takuminakayama4869@gmail.com>
2022-06-25 02:53:37 +0000

    [FINISHED] 2-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,6 +34,6 @@ class ListsController < ApplicationController
 private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
