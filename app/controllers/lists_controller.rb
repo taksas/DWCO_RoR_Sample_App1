396b563cc140ebbff0479fc22d68fc79005fb666
taksas <takuminakayama4869@gmail.com>
2022-06-25 02:19:42 +0000

    [FINISHED] 2-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,14 +9,16 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
